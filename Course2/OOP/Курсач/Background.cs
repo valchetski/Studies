1a6e2001974578cfe269dfaee0e8b4219d8bf102
VolAnder123 <saneexe@yandex.ru>
2017-06-22 13:39:56 +0300

    Add files via upload

---
+++ Course2/OOP/Курсач/Background.cs
@@ -0,0 +1,55 @@
+﻿using Django_Unchained.Levels;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Django_Unchained
+{
+    class Background : DrawableGameComponent
+    {
+        private readonly Texture2D texture2D;
+        private Rectangle rectangle1;
+        private Rectangle rectangle2;
+        private int previousCameraPosition;
+        
+        public Background(Game game, Texture2D texture2D) : base(game)
+        {
+            this.texture2D = texture2D;
+            rectangle1 = new Rectangle(0, 0, Game.GraphicsDevice.Viewport.Width, Game.GraphicsDevice.Viewport.Height);
+            rectangle2 = new Rectangle(rectangle1.Width, 0, Game.GraphicsDevice.Viewport.Width, Game.GraphicsDevice.Viewport.Height);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (rectangle1.X + rectangle1.Width <= 0)
+            {
+                rectangle1.X = rectangle2.X + rectangle1.Width;
+            }
+            if (rectangle2.X + rectangle1.Width <= 0)
+            {
+                rectangle2.X = rectangle1.X + rectangle1.Width;
+            }
+
+            if ((rectangle1.X > rectangle2.X) && (rectangle2.X > 0))
+            {
+                rectangle1.X = rectangle2.X - rectangle1.Width;
+            }
+            else if (rectangle1.X > 0)
+            {
+                rectangle2.X = rectangle1.X - rectangle1.Width;
+            }
+
+            rectangle1.X -= Level.screenLeftX - previousCameraPosition;
+            rectangle2.X -= Level.screenLeftX - previousCameraPosition;
+            previousCameraPosition = Level.screenLeftX;
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            var spriteBatch = (SpriteBatch)Game.Services.GetService(typeof(SpriteBatch));
+            spriteBatch.Draw(texture2D, rectangle1, Color.White);
+            spriteBatch.Draw(texture2D, rectangle2, Color.White);
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Course2/OOP/Курсач/Django Unchained/Django Unchained/Django Unchained/Bars/BulletsBar.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Django_Unchained.Entities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Django_Unchained.Bars
+{
+    public class BulletsBar : Entity
+    {
+        public static int allBullets;
+        public static int points;
+
+        protected readonly SpriteFont font;
+        protected string message;
+
+        public BulletsBar(Game game, Texture2D texture2D, SpriteFont font, Point coordinatesPoint, int squareSize)
+            : base(game, texture2D)
+        {
+            message = "Bullets";
+            this.font = font;
+            PositionRectangle = new Rectangle(coordinatesPoint.X * squareSize, coordinatesPoint.Y * squareSize, 0, texture2D.Height);
+            IsGravity = false;
+            isScrolling = false;
+        }
+
+        public BulletsBar(Game game)
+            : base(game, ContentContainer.bulletsBarTexture2D)
+        {
+            message = "Bullets";
+            font = ContentContainer.font;
+            IsGravity = false;
+            isScrolling = false;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            PositionRectangle = new Rectangle(PositionRectangle.Left, PositionRectangle.Top, points * 10, PositionRectangle.Bottom);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            var spriteBatch = (SpriteBatch)Game.Services.GetService(typeof(SpriteBatch));
+            spriteBatch.DrawString(font, Convert.ToString(allBullets),
+                new Vector2(PositionRectangle.Left + 100, PositionRectangle.Top), Color.Black);
+            spriteBatch.DrawString(font, points + message, new Vector2(PositionRectangle.Left, PositionRectangle.Bottom), Color.Gold);
+            base.Draw(gameTime);
+        }
+    }
+}
